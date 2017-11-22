4324931daaaca094328dec57f4d6e6d3ac4fcfec
Thomas Puppe <info@thomaspuppe.de>
2017-11-22 23:19:17 +0100

    Iterate over content files, write their content to output folder

--- run.rb
+++ run.rb
@@ -1,1 +1,17 @@
-puts 'Hello World!'
+puts 'Hello Easto!'
+
+puts 'Reading content directory'
+Dir.foreach('content') do |item|
+    next if item == '.' or item == '..'
+
+    file_path = 'content/' + item
+    puts '- found file ' + file_path
+
+    file_content = File.read(file_path)
+    puts '  - content: ' + file_content
+
+    target_path = 'output/' + item.gsub('.md', '.html')
+    target = open(target_path, 'w')
+    target.write(file_content)
+    puts '- wrote file ' + target_path
+end
