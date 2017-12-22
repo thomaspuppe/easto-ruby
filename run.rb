32bfa5d26b181564bbb5bd0e57ddd1ea86f30d07
Thomas Puppe <thomas.puppe@zeit.de>
2017-12-22 14:48:30 +0100

    Set page title from content

--- run.rb
+++ run.rb
@@ -1,5 +1,6 @@
 require 'fileutils'
 require 'redcarpet'
+require 'yaml'
 
 filecounter = 0
 Dir.foreach('content') do |item|
@@ -8,18 +9,26 @@ Dir.foreach('content') do |item|
     file_path = 'content/' + item
     file_content = File.read(file_path)
 
+    page = File.read('templates/layout.html')
+
     # Split Metadata from Body
     file_splitted = file_content.split('-----')
     file_meta = file_splitted[0]
-    
     file_body = file_splitted[1]
+
+    meta = YAML.load(file_meta)
+    meta.each do |key,value|
+        if key == 'title'
+            page = page.gsub('{{ META_TITLE }}', value)
+        end
+    end
+
     file_body = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(file_body)
 
     target_path = 'output/' + item.gsub('.md', '.html')
     target = open(target_path, 'w')
-
-    template = File.read('templates/layout.html')
-    page = template.gsub('{{ CONTENT_BODY }}', file_body)
+    
+    page = page.gsub('{{ CONTENT_BODY }}', file_body)
     target.write(page)
     filecounter += 1
 end
