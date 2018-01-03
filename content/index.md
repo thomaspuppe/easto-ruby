9a2645e2386de9d96061ea70940097bc0138967d
Thomas Puppe <info@thomaspuppe.de>
2018-01-03 22:30:06 +0100

    Create a rudimentary listing page (for blogs)

--- content/index.md
+++
@@ -1,6 +0,0 @@
-title: Welcome
-date: 2017-11-23
-
------
-
-Willkommen!
--- run.rb
+++ run.rb
@@ -3,6 +3,8 @@ require 'redcarpet'
 require 'yaml'
 
 filecounter = 0
+TEASERS = {}
+
 Dir.foreach('content') do |item|
     next if item == '.' or item == '..'
 
@@ -11,15 +13,22 @@ Dir.foreach('content') do |item|
 
     page = File.read('templates/layout.html')
 
+    teaser_html = File.read('templates/sub_index_article.html')
+
     # Split Metadata from Body
     file_splitted = file_content.split('-----')
     file_meta = file_splitted[0]
     file_body = file_splitted[1]
 
+    TEASERS[item] = teaser_html
+
     meta = YAML.load(file_meta)
     meta.each do |key,value|
         replacement_string = "{{ META_" + key.upcase + " }}"
         page = page.gsub(replacement_string, value.to_s())
+
+        TEASERS[item] = TEASERS[item].gsub(replacement_string, value.to_s())
+
     end
 
     file_body = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(file_body)
@@ -31,6 +40,20 @@ Dir.foreach('content') do |item|
     target.write(page)
     filecounter += 1
 end
+
+# concat teaser HTML
+teasers_html = TEASERS.values.join("\n\n")
+index_body = 
+index_target_path = 'output/index.html'
+index_target = open(index_target_path, 'w')
+index_page = File.read('templates/layout.html')
+index_page = index_page.gsub('{{ CONTENT_BODY }}', teasers_html)
+# just clean up for now
+index_page = index_page.gsub('{{ META_TITLE }}', '')
+index_page = index_page.gsub('{{ META_DATE }}', '')
+# ... and output it to index.html
+index_target.write(index_page)
+filecounter += 1
 
 FileUtils.copy_entry('templates/static', 'output/static')
 
